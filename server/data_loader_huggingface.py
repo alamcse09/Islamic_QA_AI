4824bbb57458e28cd8d2708ad8c577a23de83adc
alamcse09 <alam.cse09@gmail.com>
2025-12-30 12:57:33 +0100

    Add progressbar and batch ingestion

--- server/data_loader_huggingface.py
+++ server/data_loader_huggingface.py
@@ -3,6 +3,8 @@ from langchain_core.documents import Document
 from langchain_ollama import OllamaEmbeddings
 from langchain_chroma import Chroma
 import os
+from tqdm import tqdm
+import time
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(current_dir, "knowledge_HF_db")
@@ -22,14 +24,20 @@ for row in ds['train']:
 
 print(f"--- Loaded {len(documents)} documents ---")
 
-print(f"question: {documents[0]}")
-
 embeddings = OllamaEmbeddings(model="nomic-embed-text")
-vectorstore = Chroma.from_documents(
-    documents=documents,
-    embedding=embeddings,
-    persist_directory=DB_PATH,
-    collection_name="hf_knowledge"
-)
+vectorstore = None
+
+for i in tqdm(range (0, len(documents), 10), desc="Inserting Documents"):
+    batch = documents[i:i+10]
+
+    if vectorstore is None:
+        vectorstore = Chroma.from_documents(
+            documents=batch,
+            embedding=embeddings,
+            persist_directory=DB_PATH,
+            collection_name="hf_knowledge"
+        )
+    else:
+        vectorstore.add_documents(documents=batch)
 
 print("--- Ingestion Complete ---")
